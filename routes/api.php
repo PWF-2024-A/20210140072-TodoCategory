7b7a34cd94d55f1184fccc9dcc677989def17ec0
Adinda Nurhayati <adindanurhayati30@gmail.com>
2024-06-15 15:01:44 +0700

    api : create, update, delete data

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
-use App\Http\Controllers\API\ProfileController;
 use App\Http\Controllers\API\TodoController;
+use App\Http\Controllers\API\ProfileController;
+use App\Http\Controllers\API\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,9 +19,13 @@ use App\Http\Controllers\API\TodoController;
 |
 */
 Route::post('/login', [AuthController::class, 'login'])->name('login');
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware('auth:sanctum')->group(function() {
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
     //Route::resource('/todo', TodoController::class);
     Route::apiResource('/todo', TodoController::class);
+    Route::get('/category', [CategoryController::class, 'index'])->name('category.index');
+    Route::patch('/todo/{todo}/complete', [TodoController::class, 'complete'])->name('todo.complete');
+    Route::patch('/todo/{todo}/uncomplete', [TodoController::class, 'uncomplete'])->name('todo.uncomplete');
+    Route::delete('/todo', [TodoController::class, 'deleteAllCompleted'])->name('todo.deleteallcompleted');
 });
---
+++ tugas12.md
@@ -0,0 +1,65 @@
+# Tugas 12
+
+## 1. Category Index
+
+Screenshot:
+
+<div align="center">
+  <img src="screenshot/tugas12/CategoryIndex.png"/>
+</div>
+
+## 2. Todo Store
+
+Screenshot:
+
+<div align="center">
+  <img src="screenshot/tugas12/TodoStore.png" />
+</div>
+
+## 3. Todo Store Error
+
+Screenshot:
+
+<div align="center">
+  <img src="screenshot/tugas12/TodoStoreError.png"/>
+</div>
+
+## 4. Todo Update
+
+Screenshot:
+
+<div align="center">
+  <img src="screenshot/tugas12/TodoUpdate.png"/>
+</div>
+
+## 5. Todo Complete
+
+Screenshot:
+
+<div align="center">
+  <img src="screenshot/tugas12/TodoComplete.png"/>
+</div>
+
+## 6. Todo Uncomplete
+
+Screenshot:
+
+<div align="center">
+  <img src="screenshot/tugas12/TodoUncomplete.png"/>
+</div>
+
+## 7. Todo Delete
+
+Screenshot:
+
+<div align="center">
+  <img src="screenshot/tugas12/TodoDelete.png"/>
+</div>
+
+## 8. Todo Delete All Completed
+
+Screenshot:
+
+<div align="center">
+  <img src="screenshot/tugas12/TodoDeletedAllCompleted.png"/>
+</div>
